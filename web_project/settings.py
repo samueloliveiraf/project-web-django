2747e376725ea18695df6489178d3bfd05d81542
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-16 15:13:50 -0300

    acesso

--- web_project/settings.py
+++ web_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.137.139.71']
 
 # Application definition
 
